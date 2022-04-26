0a3c3c7c1b9c48c6b7b4233b99800e5e243a820e
John <johnripke@me.com>
2022-04-26 20:58:26 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import LegoContext from "../context/LegoContext"
