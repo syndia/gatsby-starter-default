6d13c1d9452ab65f1d46ebf9ed212888b16b9480
John <johnripke@me.com>
2022-04-26 21:02:47 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import LegoContext from "../context/LegoContext"
+import { LegoContext } from "../context/LegoContext"
 
 const { setChosenSet, setChosenTheme } = useContext(LegoContext);
 
