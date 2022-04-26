c0a5d66460e0d6170b2907872fcbc0c7bb5ce851
John <johnripke@me.com>
2022-04-26 20:44:50 +0200

    Update LegoContext.js

--- src/context/LegoContext.js
+++ src/context/LegoContext.js
@@ -3,6 +3,45 @@ import React, { useState, useEffect } from 'react'
 export const LegoContext = React.createContext()
 
 const LegoProvider = ({children}) => {
+  const [chosenSet, setChosenSet] = useState({})
+  const [chosenTheme, setChosenTheme] = useState('')
+  const [ownedSets, setOwnedSets] = useState([])
+
+  useEffect(() => {
+    const ownedStorageSets = JSON.parse(localStorage.getItem('ownedLegoSets'))
+    if (ownedStorageSets) {
+      setOwnedSets(ownedStorageSets)
+    }
+
+    const chosenStorageTheme = localStorage.getItem('chosenStorageTheme')
+    if (chosenStorageTheme) {
+      setChosenTheme(chosenStorageTheme)
+    }
+
+    const chosenSetStorage = JSON.parse(localStorage.getItem('chosenSetStorage'))
+    if (chosenSetStorage) {
+      setChosenSet(chosenSetStorage)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('ownedLegoSets', JSON.stringify(likedSets))
+
+    localStorage.setItem('chosenStorageTheme', chosenTheme)
+
+    localStorage.setItem('chosenSetStorage', JSON.stringify(chosenSet))
+  }, [ownedSets, chosenTheme, chosenSet])
+
+  const toggleOwned = (setNum) => {
+    const index = ownedSets.indexOf(setNum)
+    if (index > -1) {
+      ownedSets.splice(index, 1)
+      setOwnedSets([...ownedSets])
+    } else {
+      setOwnedSets([...ownedSets, setNum])
+    }
+  }
+
   return (
     <LegoContext.Provider
       value={{
@@ -10,7 +49,7 @@ const LegoProvider = ({children}) => {
            setChosenSet,
            chosenTheme
            setChosenTheme,
-           toggleOwn,
+           toggleOwned,
            ownedSets
           }}>
             {children}
