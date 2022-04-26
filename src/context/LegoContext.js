3b861926af7e52e8775f34c7448cff8ca9d71824
John <johnripke@me.com>
2022-04-26 20:34:42 +0200

    Update LegoContext.js

--- src/context/LegoContext.js
+++ src/context/LegoContext.js
@@ -2,3 +2,18 @@ import React, { useState, useEffect } from 'react'
 
 export const LegoContext = React.createContext()
 
+const LegoProvider = ({children}) => {
+  return <LegoContext.Provider
+    value={{
+           chosenSet,
+           setChosenSet,
+           chosenTheme
+           setChosenTheme,
+           toggleOwn,
+           ownedSets
+          }}>
+            {children}
+</LegoContext.Provider>
+}
+
+export default LegoProvider
